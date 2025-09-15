1ee3d32a7dccba2f9fabd906607d6c920cbcdb9a
hayboutamin-byte <elboughamin@gmail.com>
2025-09-15 14:49:09 +0100

    update challenge 12 in Day__4

--- Day__4/challenge 12.c
+++ Day__4/challenge 12.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
 
-int main()
-{
+int main(){
+
+    int T[5] = {5,2,7,87,142,14};
+    int n = 6;
+    int temp;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= 5 ;j++)
+        {
+            if (T[i]<T[j])
+            {
+                temp = T[i];
+                T[i] = T[j];
+                T[j] = temp;
+            }
+            
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",T[i]);
+        
+    }
+    
+    
     
     return 0;
 }
